a7da85db05afc1df838fb6b136df76bfa9e5b442
jitengben <997707572@qq.com>
2022-08-07 21:15:21 +0800

    feat: test git commit

--- src/utils/password-handle.js
+++ src/utils/password-handle.js
@@ -1,9 +1,9 @@
-const crypto = require('crypto');
-
+const crypto = require("crypto");
+//密码加密
 const md5password = (password) => {
-  const md5 = crypto.createHash('md5');
-  const result = md5.update(password).digest('hex');
+  const md5 = crypto.createHash("md5");
+  const result = md5.update(password).digest("hex");
   return result;
-}
+};
 
 module.exports = md5password;
